8f8fa18b979ce362f9d1b4e00dd8baf19531c3a5
Shubham Prince <44049342+princekhg@users.noreply.github.com>
2022-01-27 21:13:36 +0530

    FileReading And Writing
    Program Will create a file and all the values inside an array.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+public class FileCreateRead
+{
+    string[] arr;
+    public void read()
+    {
+        int len;
+        Console.WriteLine("Enter length of String");
+        len =Convert.ToInt32(Console.ReadLine());
+        arr = new string[len];
+        Console.WriteLine("Enter string here");
+        for (int i = 0; i < len; i++)
+            arr[i] = Console.ReadLine();
+
+    }
+    public static void Main()
+    {
+       FileCreateRead fileCreateRead = new FileCreateRead();
+        fileCreateRead.read();
+        File.WriteAllLines("new.txt", fileCreateRead.arr);
+        using (StreamReader s = new StreamReader("new.txt"))
+        {
+            string line;
+            Console.WriteLine("************************************");
+            Console.WriteLine("The file Content Will be Displayed");
+            Console.WriteLine("**************************************");
+            while ((line = s.ReadLine())!=null)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
